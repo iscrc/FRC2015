ec72c0c7b2a6f96a86b5c414528da2b02a44c085
Chris <Chris@toshiba>
2015-11-21 16:45:23 -0900

    define the general agent, ver 1

---
+++ WinAgentBasedModel/Agent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAgentBasedModel
+{
+    class Agent
+    {
+        //properties of the agent
+        //behaviors of agent
+        //access to the environment
+        //stimulus-response of agents
+    }
+}
