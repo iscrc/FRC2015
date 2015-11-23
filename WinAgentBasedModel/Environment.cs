b16d31d56ee34b339cc593cebb98e54f908eb963
Chris Curry <chris.curry@gmail.com>
2015-11-23 14:10:31 -0900

    untrack compile files

---
+++ WinAgentBasedModel/Environment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAgentBasedModel
+{
+    class Environment
+    {
+        //agents will be injected into environment
+    }
+}
