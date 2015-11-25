89eb8e3676fc13642d9bf361b0f8a8c9611a78e0
Chris Curry <chris.curry@gmail.com>
2015-11-25 14:38:58 -0900

    small code added in environment

--- WinAgentBasedModel/Environment.cs
+++ WinAgentBasedModel/Environment.cs
@@ -9,5 +9,26 @@ namespace WinAgentBasedModel
     class Environment
     {
         //agents will be injected into environment
+        List<Agent> agents;
+        List<Object> localObjects;
+
+        void addAgent(Agent a)
+        {
+            agents.Add(a);
+        }
+        //environment is stepped thru time with some increment
+        double currentTime;
+        void timeStep(double increment)
+        {
+            //adjust non-agent aspects of environment (e.g. falling objects)
+            foreach (Object o in localObjects)
+            {
+
+            }
+
+            //tells each agent to act, within the time step increment
+            foreach (Agent a in agents) { }
+            currentTime += increment;
+        }
     }
 }
